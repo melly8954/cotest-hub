8fe7d912995170c2fe34071a951e66ff5650035b
seonwoo <tjsdn8963@naver.com>
2025-12-20 18:15:02 +0900

    [level 0] Title: 배열 두배 만들기, Time: 3.29 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i<numbers.length; i++){
-             answer[i] = numbers[i] * 2;
-        }
-        return answer;
+        // 기존 풀이
+        // int[] answer = new int[numbers.length];
+        // for(int i = 0; i<numbers.length; i++){
+        //      answer[i] = numbers[i] * 2;
+        // }
+        // return answer;
+        
+        
+        // 스트림 사용 풀이
+        return Arrays.stream(numbers).map(i -> i*2).toArray();                  
     }
 }
