8a09e6c04a29cbd75cd29a76309b01734e0a79db
seonwoo <tjsdn8963@naver.com>
2025-12-20 20:38:19 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,15 +1,34 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+//         기존 풀이
+//         int numer = numer1 * denom2 + numer2 * denom1;
+//         int denom = denom1 * denom2;
+        
+//         int gcd = 1;
+//         for (int i = 1; i <= Math.min(numer, denom); i++) {
+//             if (numer % i == 0 && denom % i == 0) {
+//                 gcd = i;
+//             }
+//         }
+        
+//         return new int[]{numer / gcd, denom / gcd};
+        
+        // 유클리드 호제법 적용
         int numer = numer1 * denom2 + numer2 * denom1;
         int denom = denom1 * denom2;
-        
-        int gcd = 1;
-        for (int i = 1; i <= Math.min(numer, denom); i++) {
-            if (numer % i == 0 && denom % i == 0) {
-                gcd = i;
-            }
-        }
-        
+
+        int gcd = gcd(numer, denom);
+
         return new int[]{numer / gcd, denom / gcd};
+    }
+    
+    // 유클리드 호제법
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
     }
 }
