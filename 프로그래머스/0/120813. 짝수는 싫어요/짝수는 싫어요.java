510b0cf4ad8fb43735797d91b7191bdf4f6f55fd
seonwoo <tjsdn8963@naver.com>
2025-12-21 20:46:25 +0900

    [level 0] Title: 짝수는 싫어요, Time: 4.69 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,17 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int n) {
         List<Integer> list = new ArrayList<>();
+        
         for(int i=1; i<=n; i++) {
             if(i%2 !=0) {
                 list.add(i);
             }
         }
         
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        // 기존 방식 (for문 사용하여 배열 생성)
+        // int[] answer = new int[list.size()];
+        // for(int i=0; i<list.size(); i++) {
+        //     answer[i] = list.get(i);
+        // }
         
-        return answer;
+        return list.stream()
+                   .mapToInt(Integer::intValue)
+                   .toArray();
     }
 }
