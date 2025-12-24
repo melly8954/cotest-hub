2f28c3565bf7858806e97a48761f1d6291f18e3e
seonwoo <tjsdn8963@naver.com>
2025-12-24 18:34:43 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -8,10 +8,15 @@ class Solution {
         
         return answer;
         
-        // 다른 사람 풀이
+        // 다른 사람 풀이 1
 //         List<Integer> list = Arrays.stream(numList).boxed().collect(Collectors.toList());
 
 //         Collections.reverse(list);
 //         return list.stream().mapToInt(Integer::intValue).toArray();
+        
+        // 다른 사람 풀이 2
+        // return IntStream.range(0, num_list.length)
+        //                 .map(i -> num_list[num_list.length - 1 - i])
+        //                 .toArray();
     }
 }
