5c990eba8f530f4dcf83d72f0cf7fc98dbcb6022
seonwoo <tjsdn8963@naver.com>
2025-12-24 18:09:51 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int price) {
         if(price >=500000) {
-            double result = price * 0.8;
-            return (int) result;
+            return (int)(price * 0.8);
         } else if(price >=300000) {
-            double result = price * 0.9;
-            return (int) result;
+            return (int)(price * 0.9);
         } else if(price >=100000) {
-            double result = price * 0.95;
-            return (int) result;
+            return (int)(price * 0.95);
         } else {
             return price;
         }
