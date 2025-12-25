6790901ce8c2ba5b7db766b16c553031281bb589
seonwoo <tjsdn8963@naver.com>
2025-12-25 18:44:37 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int hol_count = 0;
-        int zzak_count = 0;
         int[] answer = new int[2];
         
         for(int num : num_list) {
-            if(num % 2 != 0) {
-                hol_count++;
+            if(num % 2 == 0) {
+                answer[0]++;
             }else {
-                zzak_count++;
+                answer[1]++;
             }
         }
-        answer[0] = zzak_count;
-        answer[1] = hol_count;
                
         return answer;
     }
