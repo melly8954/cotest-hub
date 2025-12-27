191162d0bfa632750c94053702aae6f9f24a1a3c
seonwoo <tjsdn8963@naver.com>
2025-12-27 21:13:15 +0900

    [level 0] Title: 순서쌍의 개수, Time: 9.83 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,12 +1,17 @@
+import java.util.stream.*;
+
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+//         int answer = 0;
         
-        for(int i = 1; i<=n; i++){
-            if(n%i == 0){
-                answer++;
-            }
-        }
-        return answer;
+//         for(int i = 1; i<=n; i++){
+//             if(n%i == 0){
+//                 answer++;
+//             }
+//         }
+//         return answer;
+        
+        // stream
+        return (int) IntStream.rangeClosed(1,n).filter(i -> n%i == 0).count();
     }
 }
