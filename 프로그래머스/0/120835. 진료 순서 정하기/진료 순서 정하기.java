d4f1d5512ab869af2b9e166bc4ed49e5ec4f8160
seonwoo <tjsdn8963@naver.com>
2025-12-27 21:51:33 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
@@ -16,5 +18,24 @@ class Solution {
         }
         
         return answer;
+        
+        // 정렬 + Map 방식
+//         int[] answer = new int[emergency.length];
+//         int[] sort = Arrays.copyOf(emergency, emergency.length);
+        
+//         Arrays.sort(sort);
+        
+//         Map<Integer, Integer> rank = new HashMap<Integer, Integer>();
+//         int rankIdx = 1;
+//         for (int idx = sort.length - 1; idx >= 0; idx--) {
+//             rank.put(sort[idx], rankIdx);
+// 			rankIdx++;
+//         }
+        
+//         for (int idx = 0; idx < emergency.length; idx++) {
+//             answer[idx] = rank.get(emergency[idx]);
+//         }
+        
+//         return answer;
     }
 }
