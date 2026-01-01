48b16046efe884f1def11b688b2f5ba681755b89
seonwoo <tjsdn8963@naver.com>
2026-01-01 15:58:01 +0900

    [level 0] Title: 공 던지기, Time: 0.06 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -14,5 +14,29 @@ class Solution {
         }
         
         return answer;
+        
+        // 다른 사람 풀이
+        // int index = ((k - 1) * 2) % numbers.length;
+        // return numbers[index];
+        
+        // 원형 큐
+//         Queue<Integer> queue = new LinkedList<>();
+        
+//         for (int num : numbers) {
+//             queue.offer(num);
+//         }
+
+//         int answer = 0;
+
+//         for (int i = 0; i < k; i++) {
+//             // 공 받는 사람
+//             answer = queue.poll();   
+
+//             // 두 명 건너뛰기
+//             queue.offer(queue.poll());
+//             queue.offer(queue.poll());
+//         }
+
+//         return answer;
     }
 }
