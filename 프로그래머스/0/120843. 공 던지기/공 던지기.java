69df451857ff796a8b438dfdc0b5725cf168e6b1
seonwoo <tjsdn8963@naver.com>
2026-01-01 18:38:12 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,25 +1,29 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 0;
-        int idx = 0;
-        int cnt = 0;
-        
-        while(cnt < k){
-            if(idx >= numbers.length){
+        int idx = 0;   // 공을 받을 사람의 인덱스
+        int cnt = 0;   // 공을 던진 횟수
+
+        // k번 공을 던질 때까지 반복
+        while (cnt < k) {
+
+            // 인덱스가 배열 길이를 넘으면 처음으로 되돌림 (원형 구조)
+            if (idx >= numbers.length) {
                 idx %= numbers.length;
             }
-            answer = numbers[idx];
-            idx += 2;
-            cnt++;
+
+            answer = numbers[idx]; // 현재 공을 받는 사람
+            idx += 2;              // 한 명 건너뛰고 다음 사람
+            cnt++;                 // 던진 횟수 증가
         }
         
         return answer;
         
-        // 다른 사람 풀이
-        // int index = ((k - 1) * 2) % numbers.length;
-        // return numbers[index];
+//         다른 사람 풀이
+//         int index = ((k - 1) * 2) % numbers.length;
+//         return numbers[index];
         
-        // 원형 큐
+//         원형 큐
 //         Queue<Integer> queue = new LinkedList<>();
         
 //         for (int num : numbers) {
