1053c7ad62e6119adcf38515b0ccbb3a1b2d8a0b
seonwoo <tjsdn8963@naver.com>
2026-01-03 20:56:42 +0900

    [level 0] Title: 모음 제거, Time: 0.17 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,24 +1,26 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+//         String answer = "";
         
-        answer = checkWord(my_string);
+//         answer = checkWord(my_string);
        
-        return answer;
+//         return answer;
+        
+        return my_string.replaceAll("[aeiou]", "");
     }
     
-    private String checkWord(String my_string){
-        String result = "";
+//     private String checkWord(String my_string){
+//         String result = "";
         
-        for(int i = 0; i < my_string.length(); i++){
-            char word = my_string.charAt(i);            
+//         for(int i = 0; i < my_string.length(); i++){
+//             char word = my_string.charAt(i);            
             
-            if (word == 'a' || word == 'e' || word == 'i' || word == 'o' || word == 'u') {
-                continue;
-            }
+//             if (word == 'a' || word == 'e' || word == 'i' || word == 'o' || word == 'u') {
+//                 continue;
+//             }
             
-            result += word;
-        }
-        return result;
-    }
+//             result += word;
+//         }
+//         return result;
+//     }
 }
