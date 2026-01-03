c359907dd567f87013c7995738e0547b08891279
seonwoo <tjsdn8963@naver.com>
2026-01-03 21:27:53 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.36 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -4,24 +4,21 @@ class Solution {
     public int[] solution(String my_string) {
         List<Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < my_string.length(); i++){
-            char word = my_string.charAt(i);
-            if(checkNum(word)){
-                list.add(word - '0');
-            }
+        String[] strArr = my_string.replaceAll("[a-zA-Z]","").split("");
+        
+        for(int i = 0; i < strArr.length; i++){
+            String word = strArr[i];
+            list.add(Integer.parseInt(word));
         }
         
         Collections.sort(list);
         
         int[] answer = new int[list.size()];
+        
         for(int i = 0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
         
         return answer;        
-    }
-    
-    private boolean checkNum(char word){
-        return word >= '0' && word <= '9';
     }
 }
