2ca59b7ead2b0f4618380986a53a0c889d1ee679
seonwoo <tjsdn8963@naver.com>
2026-01-03 20:53:56 +0900

    [level 0] Title: 모음 제거, Time: 9.98 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        answer = checkWord(my_string);
+       
+        return answer;
+    }
+    
+    private String checkWord(String my_string){
+        String result = "";
+        
+        for(int i = 0; i < my_string.length(); i++){
+            char word = my_string.charAt(i);            
+            
+            if (word == 'a' || word == 'e' || word == 'i' || word == 'o' || word == 'u') {
+                continue;
+            }
+            
+            result += word;
+        }
+        return result;
+    }
+}
