8b8558f495d2d0a88011b915a0bf23e3ece6c060
seonwoo <tjsdn8963@naver.com>
2026-01-06 19:02:13 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.42 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,15 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        String[] strArr = s.split(" ");
+//         int answer = 0;
+//         String[] strArr = s.split(" ");
         
-        for(int i = 0; i < strArr.length; i++){
-            if(strArr[i].equals("Z")){
-                answer -= Integer.parseInt(strArr[i-1]);
-            } else{
-                answer += Integer.parseInt(strArr[i]);
-            }
+//         for(int i = 0; i < strArr.length; i++){
+//             if(strArr[i].equals("Z")){
+//                 answer -= Integer.parseInt(strArr[i-1]);
+//             } else{
+//                 answer += Integer.parseInt(strArr[i]);
+//             }
             
+//         }
+//         return answer;
+        
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (String w : s.split(" ")) {
+            if (w.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(w));
+            }
+        }
+        for (int i : stack) {
+            answer += i;
         }
         return answer;
     }
