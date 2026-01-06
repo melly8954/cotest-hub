90bd2833fc4f42275179f7f9b5b72ed8cf79b74d
seonwoo <tjsdn8963@naver.com>
2026-01-06 21:15:14 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -2,15 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int order) {
+//         int answer = 0;
+//         long n = 0;
+        
+//         String str = String.valueOf(order);
+        
+//         for(int i = 0; i < str.length(); i++){
+//             n = Character.getNumericValue(str.charAt(i)); // str.charAt(i) - '0'
+            
+//             if(n!= 0 && n % 3 == 0){
+//                 answer ++;
+//             }
+//         }
+        
+//         return answer;
         int answer = 0;
-        long n = 0;
         
         String str = String.valueOf(order);
         
         for(int i = 0; i < str.length(); i++){
-            n = Character.getNumericValue(str.charAt(i)); // str.charAt(i) - '0'
+            char c = str.charAt(i);
             
-            if(n!= 0 && n % 3 == 0){
+            if(c == '3' || c == '6' || c == '9'){
                 answer ++;
             }
         }
