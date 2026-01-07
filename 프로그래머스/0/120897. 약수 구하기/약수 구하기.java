9f84a5f1376535f3ceb3586b8d39ef74ff15d7ef
seonwoo <tjsdn8963@naver.com>
2026-01-07 16:52:05 +0900

    [level 0] Title: 약수 구하기, Time: 3.63 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                list.add(i);    
+            }
+            
+        }
+        
+        Collections.sort(list);
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
