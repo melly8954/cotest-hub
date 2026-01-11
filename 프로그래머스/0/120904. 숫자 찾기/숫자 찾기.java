768c775de780abf6c06eb126e8c7bb0997e2ee1b
seonwoo <tjsdn8963@naver.com>
2026-01-11 17:51:49 +0900

    [level 0] Title: 숫자 찾기, Time: 9.25 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,22 +1,25 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
+//         int answer = 0;
         
-        String num_str = String.valueOf(num);
+//         String num_str = String.valueOf(num);
         
-        String[] num_arr = num_str.split("");
+//         String[] num_arr = num_str.split("");
         
-        for(int i = 0; i < num_arr.length; i++){
-            int n = Integer.parseInt(num_arr[i]);
+//         for(int i = 0; i < num_arr.length; i++){
+//             int n = Integer.parseInt(num_arr[i]);
             
-            if(n == k){
-                answer = i+1;
-                break;
-            } else{
-                answer = -1;
-            }
-        }
+//             if(n == k){
+//                 answer = i+1;
+//                 break;
+//             } else{
+//                 answer = -1;
+//             }
+//         }
         
-        return answer;
+//         return answer;
+        
+        // 코드 1줄 풀이
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
