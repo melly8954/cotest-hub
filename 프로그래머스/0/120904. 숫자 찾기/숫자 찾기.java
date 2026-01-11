1e7693604bfe73909b1a7c28b786580cc2b6e5ff
seonwoo <tjsdn8963@naver.com>
2026-01-11 17:47:10 +0900

    [level 0] Title: 숫자 찾기, Time: 0.16 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        
+        String num_str = String.valueOf(num);
+        
+        String[] num_arr = num_str.split("");
+        
+        for(int i = 0; i < num_arr.length; i++){
+            int n = Integer.parseInt(num_arr[i]);
+            
+            if(n == k){
+                answer = i+1;
+                break;
+            } else{
+                answer = -1;
+            }
+        }
+        
+        return answer;
+    }
+}
