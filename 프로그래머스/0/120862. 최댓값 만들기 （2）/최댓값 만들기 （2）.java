60a3a348dc598d32df5d68e5198100420cb94754
seonwoo <tjsdn8963@naver.com>
2026-01-23 16:03:50 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.56 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
+
+        Arrays.sort(numbers);
+
+        for(int i = 0; i < numbers.length; i++){
+            answer = Math.max(
+                    numbers[0] * numbers[1],
+                    numbers[numbers.length - 2] * numbers[numbers.length - 1]
+            );
+        }
+
         return answer;
     }
 }
