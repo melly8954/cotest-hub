0da28cdb3eb538467ae2abf1cf89bc38dc11ff24
seonwoo <tjsdn8963@naver.com>
2026-01-23 15:25:06 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -4,21 +4,22 @@ class Solution {
         answer[0] = 0;
         answer[1] = 0;
 
-        int max1 = (int) (board[0] / 2);
-        int max2 = (int) (board[1] / 2);
+        int maxX = (int) (board[0] / 2);
+        int maxY = (int) (board[1] / 2);
 
         for(String k : keyinput){
-            if(k.equals("left") && answer[0] > -max1){
+            if(k.equals("left") && answer[0] > -maxX){
                 answer[0]--;
-            } else if(k.equals("right") && answer[0] < max1){
+            } else if(k.equals("right") && answer[0] < maxX){
                 answer[0]++;
-            } else if(k.equals("up") && answer[1] < max2){
+            } else if(k.equals("up") && answer[1] < maxY){
                 answer[1]++;
-            } else if(k.equals("down") && answer[1] > -max2){
+            } else if(k.equals("down") && answer[1] > -maxY){
                 answer[1]--;
             }
         }
-        
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
         return answer;
     }
 }
