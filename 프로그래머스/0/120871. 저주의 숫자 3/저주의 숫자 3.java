882a5b8ab73289747e05eeab7e1f9c102f5ded3a
seonwoo <tjsdn8963@naver.com>
2026-02-02 21:43:46 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.24 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(int n) {
-        int num = 0;
-        int cnt = 0;
-
-        while (cnt < n) {
-            num++;
-
-            // 3의 배수이거나 숫자에 3이 포함되면 제외
-            if (num % 3 == 0 || String.valueOf(num).contains("3")) {
+        int order = 0;        
+        int villageNumber = 0;
+        
+        while(order < n) {
+            villageNumber ++;
+            
+            if(villageNumber % 3 == 0 || String.valueOf(villageNumber).contains("3")){
                 continue;
             }
-
-            // 조건을 만족하면 카운트 증가
-            cnt++;
+        
+            order ++;
         }
-
-        return num;
+        
+        return villageNumber;
     }
 }
