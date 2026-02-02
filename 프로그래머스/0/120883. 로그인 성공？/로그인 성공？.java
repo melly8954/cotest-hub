853d7ea94a071e1a3151265a69d03346c97728b4
seonwoo <tjsdn8963@naver.com>
2026-02-02 20:34:07 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String id = id_pw[0];
+        String pw = id_pw[1];
+        
+        for(int i = 0; i < db.length; i++) {
+            for(int j = 0; j < db[i].length; j++) {
+                if(id.equals(db[i][0]) && pw.equals(db[i][1])) {
+                    return "login";
+                } else if(id.equals(db[i][0]) && !pw.equals(db[i][1])) {
+                    return "wrong pw";
+                }
+            }
+        }
+        return "fail";
+    }
+}
