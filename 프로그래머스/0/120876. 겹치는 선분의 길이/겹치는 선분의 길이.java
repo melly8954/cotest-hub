af1fb357b835ae1d148381255edeba51ffd8c787
seonwoo <tjsdn8963@naver.com>
2026-02-03 20:11:04 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -5,11 +5,13 @@ class Solution {
         // 좌표 범위: -100 ~ 100 → 0 ~ 200 보정
         int[] count = new int[201];
 
-        // 각 선분 표시
+        // 각 선분 순회
         for (int i = 0; i < 3; i++) {
             int start = lines[i][0] + 100;
             int end   = lines[i][1] + 100;
-
+            
+            // start부터 end 직전까지의 각 정수 구간을 지나가며
+            // 해당 구간을 포함하는 선분의 개수를 카운팅 
             for (int j = start; j < end; j++) {
                 count[j]++;
             }
