97ec0b06a4bc946ed11ddf23cc68a380ae5814e0
seonwoo <tjsdn8963@naver.com>
2026-02-08 16:41:33 +0900

    [level 0] Title: k의 개수, Time: 3.46 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,16 +1,34 @@
 class Solution {
     public int solution(int i, int j, int k) {
+//         int answer = 0;
+
+//         for(int idx = i; idx <= j; idx++) {
+//             String s = String.valueOf(idx); 
+
+//             String[] token = s.split("");
+
+//             for(String st : token) {
+//                 if(st.contains(String.valueOf(k))){
+//                     answer++;
+//                 }
+//             }
+//         }
+
+//         return answer;
+        
+        // 산술 연산 풀이
         int answer = 0;
 
-        for(int idx = i; idx <= j; idx++) {
-            String s = String.valueOf(idx); 
+        for (int n = i; n <= j; n++) {
+            int num = n;
 
-            String[] token = s.split("");
+            
 
-            for(String st : token) {
-                if(st.contains(String.valueOf(k))){
+            while (num > 0) {
+                if (num % 10 == k) {
                     answer++;
                 }
+                num /= 10;
             }
         }
 
