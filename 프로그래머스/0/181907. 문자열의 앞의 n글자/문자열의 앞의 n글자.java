1be59b8b0abbc4ae46351bebced73a3cebca4086
seonwoo <tjsdn8963@naver.com>
2026-02-11 20:28:32 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.13 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        char[] ch = my_string.toCharArray();
+        
+        for (int i = 0; i < n; i++) {
+            sb.append(ch[i]);    
+        }
+        
+        
+        return sb.toString();
+    }
+}
