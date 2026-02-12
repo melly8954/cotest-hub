7fce01f1960ec3af19c46cea2bd073f317522edf
seonwoo <tjsdn8963@naver.com>
2026-02-12 18:47:40 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,15 +1,32 @@
 class Solution {
     public boolean solution(int x) {
+//         boolean answer = false;
+//         int sum = 0;
+        
+//         String[] str = String.valueOf(x).split("");
+        
+//         for (String token : str) {
+//             sum += Integer.parseInt(token);
+//         }
+        
+//         if (x % sum == 0) {
+//             answer = true;
+//         }
+        
+//         return answer;
+        
+        // 숫자로 진행하기
         boolean answer = false;
+        int origin = x;
         int sum = 0;
         
-        String[] str = String.valueOf(x).split("");
-        
-        for (String token : str) {
-            sum += Integer.parseInt(token);
+        while (x > 0) {
+            sum += x % 10;
+            
+            x /= 10;
         }
         
-        if (x % sum == 0) {
+        if (origin % sum == 0) {
             answer = true;
         }
         
