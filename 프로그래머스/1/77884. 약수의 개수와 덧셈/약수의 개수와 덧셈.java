df65808984a95c57fd23d29fc98aa4fd95808418
seonwoo <tjsdn8963@naver.com>
2026-02-15 17:58:34 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.34 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -15,19 +15,35 @@ class Solution {
         return sum;
     }
     
+//     private int getDivide(int n) {
+//         int cnt = 0;
+//         int idx = 1;
+        
+//         while (idx <= n) {
+//             if (n % idx == 0) {
+//                 cnt ++;
+//             }
+            
+//             idx ++;
+//         }
+        
+//         return cnt;
+//     }
+    
+    // 완전 제곱 수 방식
     private int getDivide(int n) {
         int cnt = 0;
-        int idx = 1;
-        
-        while (idx <= n) {
-            if (n % idx == 0) {
-                cnt ++;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i * i == n) {
+                    cnt++;        // 완전제곱수
+                } else {
+                    cnt += 2;     // (i, n/i)
+                }
             }
-            
-            idx ++;
         }
-        
-        return cnt;
-    }
     
+        return cnt;
+    }    
 }
