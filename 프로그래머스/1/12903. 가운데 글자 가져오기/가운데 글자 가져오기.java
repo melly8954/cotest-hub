909eb29431ed30c382114b1303979a71042495be
seonwoo <tjsdn8963@naver.com>
2026-02-15 16:47:51 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,25 +1,35 @@
 class Solution {
     public String solution(String s) {
-        char[] ch = s.toCharArray();
-        char c;
+        // 기존 풀이
+//         char[] ch = s.toCharArray();
+//         char c;
         
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < ch.length; i++) {
+//         for (int i = 0; i < ch.length; i++) {
             
-        }
+//         }
+        
+//         if (s.length() % 2 == 0) {
+//             c = ch[ch.length / 2 - 1];
+//             sb.append(c);
+            
+//             c = ch[ch.length / 2];
+//             sb.append(c);
+//         } else {
+//             c = ch[ch.length / 2];
+//             sb.append(c);
+//         }
+        
+//         return sb.toString();
+        
+        // substring() 사용
+        int mid = s.length() / 2;
         
         if (s.length() % 2 == 0) {
-            c = ch[ch.length / 2 - 1];
-            sb.append(c);
-            
-            c = ch[ch.length / 2];
-            sb.append(c);
+            return s.substring(mid - 1, mid + 1);
         } else {
-            c = ch[ch.length / 2];
-            sb.append(c);
+            return s.substring(mid, mid + 1);
         }
-        
-        return sb.toString();
     }
 }
