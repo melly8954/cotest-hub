076660448f092e56574b7d757a2171ca2a3bafdb
seonwoo <tjsdn8963@naver.com>
2026-02-15 17:31:32 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 11.85 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -3,22 +3,30 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
 
-        for (int n : arr) {
-            list.add(n);
+//         for (int n : arr) {
+//             list.add(n);
+//         }
+
+//         int min = list.stream().min((a, b) -> a - b).get();
+//         // int min2 = list.stream().mapToInt(n -> n).min().getAsInt();
+
+//         list.remove(Integer.valueOf(min));
+
+//         if (list.size() == 0) {
+//             list.add(-1);
+//             return list.stream().mapToInt(n -> n).toArray();
+//         } else {
+//             return list.stream().mapToInt(n -> n).toArray();
+//         }
+        
+        // 배열 그대로 풀기
+        if (arr.length == 1) {
+            return new int[]{-1};
         }
+        int min = Arrays.stream(arr).min().getAsInt();
 
-        int min = list.stream().min((a, b) -> a - b).get();
-        // int min2 = list.stream().mapToInt(n -> n).min().getAsInt();
-
-        list.remove(Integer.valueOf(min));
-
-        if (list.size() == 0) {
-            list.add(-1);
-            return list.stream().mapToInt(n -> n).toArray();
-        } else {
-            return list.stream().mapToInt(n -> n).toArray();
-        }
+        return Arrays.stream(arr).filter(n -> n != min).toArray();
     }
 }
