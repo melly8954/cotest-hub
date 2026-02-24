b5e70ee707c98a811b8f5b4527b96d3199435e79
seonwoo <tjsdn8963@naver.com>
2026-02-24 21:55:00 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,17 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] number) {
-        int cnt = 0;
+//         int cnt = 0;
         
-        for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i + 1; j < number.length - 1; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        cnt ++;
-                    }
-                }
-            }
+//         for (int i = 0; i < number.length - 2; i++) {
+//             for (int j = i + 1; j < number.length - 1; j++) {
+//                 for (int k = j + 1; k < number.length; k++) {
+//                     if (number[i] + number[j] + number[k] == 0) {
+//                         cnt ++;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return cnt;
+        
+        
+        // 다른 풀이 
+        return combination(number, 0, 0, 0);
+    }
+    
+     private int combination(int[] number, int index, int limit, int sum) {
+        if (limit == 3) {
+            return sum == 0 ? 1 : 0;
         }
-        
-        return cnt;
+
+        int count = 0;
+        for (int i = index; i < number.length; i++) {
+            count += combination(number, i + 1, limit + 1, sum + number[i]);
+        }
+        return count;
     }
 }
