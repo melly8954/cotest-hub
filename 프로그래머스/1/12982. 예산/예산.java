24ece4cd58680b3b2010a01e0e0b78d84692325e
seonwoo <tjsdn8963@naver.com>
2026-02-27 14:02:16 +0900

    [level 1] Title: 예산, Time: 1.30 ms, Memory: 98.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int sum = 0;
+        int cnt = 0;
+        
+        Arrays.sort(d);
+        
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            
+            if (sum <= budget) {
+                cnt ++;
+            } else {
+                break;
+            }   
+        }
+        
+        return cnt;
+    }
+}
