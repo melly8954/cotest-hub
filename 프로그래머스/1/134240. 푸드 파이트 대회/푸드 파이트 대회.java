6fd14512d5506879a85fa5d4c9bbc263496eccd0
seonwoo <tjsdn8963@naver.com>
2026-03-01 16:02:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.78 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,26 +2,43 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
         
-        Stack<String> stack = new Stack<>();
+//         Stack<String> stack = new Stack<>();
+        
+//         for (int i = 1; i < food.length; i++) {
+//             if (food[i] / 2 > 0) {
+//                 for (int j = 0; j < food[i] / 2; j++) {
+//                     stack.push(String.valueOf(i));
+//                     sb.append(i);
+//                 }
+//             }
+//         }
+        
+//         sb.append("0");
+        
+//         while (!stack.isEmpty()) {
+//             String n = stack.pop();
+//             sb.append(n);
+//         }
+        
+//         return sb.toString();
+        
+        
+        // reverser 메서드 사용
+        StringBuilder sb = new StringBuilder();
         
         for (int i = 1; i < food.length; i++) {
             if (food[i] / 2 > 0) {
                 for (int j = 0; j < food[i] / 2; j++) {
-                    stack.push(String.valueOf(i));
                     sb.append(i);
                 }
             }
         }
         
-        sb.append("0");
+        String left = sb.toString();
+        String reverse = sb.reverse().toString();
         
-        while (!stack.isEmpty()) {
-            String n = stack.pop();
-            sb.append(n);
-        }
-        
-        return sb.toString();
-    }
+        return left + "0" + reverse;  
+    }  
 }
