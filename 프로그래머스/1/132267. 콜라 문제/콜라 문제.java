54507d3e58d930c6ff7b83bc1025b4e8b0858804
seonwoo <tjsdn8963@naver.com>
2026-03-04 18:47:50 +0900

    [level 1] Title: 콜라 문제, Time: 0.13 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int a, int b, int n) {
         int sum = 0;
         int total_can = n;
-        int new_vacant = 0;
+        int new_cola = 0;
         
-        while (total_can >= a) {
-            new_vacant = (total_can / a) * b;
-            total_can = new_vacant + (total_can % a);
-            sum += new_vacant;
+        while (total_can >= a) {    // a개 이상 있어야 새 콜라로 교환 가능
+            new_cola = (total_can / a) * b;     // 새로 받은 콜라
+            total_can = new_cola + (total_can % a); // 새로 받은 콜라 + 남은 빈 병 
+            sum += new_cola;
         }
         
         return sum;
