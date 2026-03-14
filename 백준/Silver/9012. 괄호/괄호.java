c622a078bb905200f8297c12343c99f6fd71cecc
seonwoo <tjsdn8963@naver.com>
2026-03-14 16:58:23 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,11 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -20,7 +15,7 @@ public class Main {
             boolean check = true;
 
             for (int i = 0; i < input.length(); i++) {
-                if ( '(' == input.charAt(i)) {
+                if ('(' == input.charAt(i)) {
                     stack.push(input.charAt(i));
                 } else {
                     if (stack.isEmpty()) {
