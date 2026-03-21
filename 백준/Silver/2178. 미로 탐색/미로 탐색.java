ae6ce735a2fd9c534c3eb41ed75e6441bfed8b26
seonwoo <tjsdn8963@naver.com>
2026-03-22 00:36:50 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14796 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -16,15 +16,14 @@ public class Main {
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());   // 세로
+        m = Integer.parseInt(st.nextToken());   // 가로
 
-        maze = new int[n][m];
-        dist = new int[n][m];
+        maze = new int[n][m];   // 미로 크기(n * m)
+        dist = new int[n][m];   // 해당 구역[n][m]까지의 최소거리
 
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-
+            String s = br.readLine();   // 한줄 읽기
             for (int j = 0; j < m; j++) {
                 maze[i][j] = s.charAt(j) - '0';
             }
@@ -35,32 +34,40 @@ public class Main {
 
     static int bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
         dist[x][y] = 1;
+        queue.offer(new int[]{x, y});
 
         while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int curX = now[0];
-            int curY = now[1];
+            int[] select = queue.poll();
+            int curX = select[0];
+            int curY = select[1];
 
             // 목적지(n, m)에 도착했다면 즉시 거리 반환
             if (curX == n - 1 && curY == m - 1) {
                 return dist[curX][curY];
             }
 
+            // 상하좌우 좌표(4방향) 탐색
             for (int d = 0; d < 4; d++) {
-                int nowX = curX + moveX[d];
-                int nowY = curY + moveY[d];
+                int nextX = curX + moveX[d];
+                int nextY = curY + moveY[d];
 
                 // 조건 체크
-                if ((nowX >= 0 && nowX < n && nowY >= 0 && nowY < m)
-                        && maze[nowX][nowY] == 1 && dist[nowX][nowY] == 0) {
-                    dist[nowX][nowY] = dist[curX][curY] + 1;
-                    queue.offer(new int[]{nowX, nowY});
-                }
+                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
+
+                // 이동 불가 검사
+                if (maze[nextX][nextY] == 0) continue;
+
+                // 방문 검사
+                if (dist[nextX][nextY] != 0) continue;
+
+                // 거리 갱신 및 큐 추가
+                dist[nextX][nextY] = dist[curX][curY] + 1;
+                queue.offer(new int[]{nextX, nextY});
+
             }
         }
-
+        
         return 0;
     }
 }
