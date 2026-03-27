a53fb577a5afec15334d4d2b59050f3eea0d73b8
seonwoo <tjsdn8963@naver.com>
2026-03-28 01:56:31 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 183832 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,33 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static int row, col;
     static int[][] container;
 
-    static int[] moveX = {1, -1, 0, 0};
-    static int[] moveY = {0, 0, 1, -1};
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        col = Integer.parseInt(st.nextToken()); // 가로
-        row = Integer.parseInt(st.nextToken()); // 세로
+        col = Integer.parseInt(st.nextToken());
+        row = Integer.parseInt(st.nextToken());
 
         container = new int[row][col];
+        Queue<Tomato> queue = new LinkedList<>();
 
-        Queue<int[]> queue = new LinkedList<>();
-
-        // 처음 익어 있는 토마토 구하기
         for (int i = 0; i < row; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < col; j++) {
                 container[i][j] = Integer.parseInt(st.nextToken());
 
                 if (container[i][j] == 1) {
-                    queue.add(new int[]{i, j}); // 익은 토마토 시작 좌표 큐에 세팅
+                    queue.offer(new Tomato(i,j));
                 }
             }
         }
@@ -36,42 +35,52 @@ public class Main {
 
         int day = 0;
 
-        // 전체 탐색
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                // 익지 못한 토마토가 있으면 실패
                 if (container[i][j] == 0) {
                     System.out.println(-1);
+
                     return;
                 }
-                // 매핑된 값 중 가장 큰 값 고르기
+
                 day = Math.max(day, container[i][j]);
             }
         }
 
-        // 0일차 매핑이 1로 되어있기 때문에
         System.out.println(day - 1);
     }
 
-    static void bfs(Queue<int[]> q) {
-        while (!q.isEmpty()) {
-            int[] select = q.poll();
-            int curX = select[0];
-            int curY = select[1];
+    static class Tomato {
+        int x;
+        int y;
+
+        public Tomato(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static void bfs(Queue<Tomato> queue) {
+        while (!queue.isEmpty()) {
+            Tomato select = queue.poll();
+
+            int curX = select.x;
+            int curY = select.y;
+
+            int[] moveX = new int[] {1, -1, 0, 0};
+            int[] moveY = new int[] {0, 0, 1, -1};
 
             for (int i = 0; i < 4; i++) {
                 int nextX = curX + moveX[i];
                 int nextY = curY + moveY[i];
 
-                // 범위 체크
                 if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col) {
                     continue;
                 }
 
-                // 안 익은 토마토라면
                 if (container[nextX][nextY] == 0) {
                     container[nextX][nextY] = container[curX][curY] + 1;
-                    q.add(new int[]{nextX, nextY});
+                    queue.offer(new Tomato(nextX, nextY));
                 }
             }
         }
