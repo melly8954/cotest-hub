d8499858457d09861248f8da5ff31b558a83e9f4
seonwoo <tjsdn8963@naver.com>
2026-05-05 19:48:50 +0900

    [level 2] Title: 주식가격, Time: 17.87 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i = 0; i < prices.length; i++) {
+            int time = 0;
+            
+            for (int j = i + 1; j < prices.length; j++) {
+                time++;
+
+                if (prices[j] < prices[i]) {
+                    break; 
+                }
+            }
+
+            answer[i] = time;
+        }
+
+        return answer;
+    }
+}
